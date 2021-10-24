0298603f0f38db5704b4010de166a7d2d76d0062
Kevin Huffman <huffmanks2@gmail.com>
2021-10-23 20:17:01 -0400

    final

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const toggleSwitch = document.querySelector('#toggle-switch')
 const toggleSwitchBall = document.querySelector('.toggle-switch-after')
 const toggleOptions = document.querySelectorAll('.toggle-options span')
-const keyEquals = document.querySelector('.key-equals')
 
+// Change Theme
 toggleSwitch.addEventListener('click', (e) => {
     if (e.clientX - toggleSwitch.offsetLeft < 16) {
         updateTheme1()
     }
-    if (
-        e.clientX - toggleSwitch.offsetLeft > 15 &&
-        e.clientX - toggleSwitch.offsetLeft < 32
-    ) {
+    if (e.clientX - toggleSwitch.offsetLeft > 15 && e.clientX - toggleSwitch.offsetLeft < 32) {
         updateTheme2()
     }
     if (e.clientX - toggleSwitch.offsetLeft > 31) {
@@ -32,155 +29,154 @@ toggleOptions.forEach((toggleOption) => {
     })
 })
 
+// Update Theme
 const updateTheme1 = () => {
     toggleSwitchBall.style.left = '2px'
     toggleSwitchBall.style.transform = 'translateX(0%)'
-    document.documentElement.style.setProperty(
-        '--color-primary',
-        'hsl(0, 0%, 100%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-primary',
-        'hsl(222, 26%, 31%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-screen',
-        'hsl(224, 36%, 15%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-keypad',
-        'hsl(223, 31%, 20%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary',
-        'hsl(30, 25%, 89%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary-shadow',
-        'hsl(28, 16%, 65%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary',
-        'hsl(225, 21%, 49%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary-shadow',
-        'hsl(224, 28%, 35%)'
-    )
-    keyEquals.style.color = 'hsl(0, 0%, 100%)'
-    document.documentElement.style.setProperty(
-        '--theme-key-equals',
-        'hsl(6, 63%, 50%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-equals-shadow',
-        'hsl(6, 70%, 34%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-color-primary',
-        'hsl(221, 14%, 31%)'
-    )
-    document.querySelector('.attribution').style.color = 'hsl(0, 0%, 0%)'
+    const cls = ['theme-2', 'theme-3']
+    document.body.classList.add('theme-1')
+    document.body.classList.remove(...cls)
 }
 
 const updateTheme2 = () => {
     toggleSwitchBall.style.left = 'calc(61% - 3px)'
     toggleSwitchBall.style.transform = 'translateX(-61%)'
-    document.documentElement.style.setProperty(
-        '--color-primary',
-        'hsl(60, 10%, 19%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-primary',
-        'hsl(0, 0%, 90%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-screen',
-        'hsl(0, 0%, 93%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-keypad',
-        'hsl(0, 5%, 81%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary',
-        'hsl(45, 7%, 89%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary-shadow',
-        'hsl(35, 11%, 61%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary',
-        'hsl(185, 42%, 37%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary-shadow',
-        'hsl(185, 58%, 25%)'
-    )
-    keyEquals.style.color = 'hsl(0, 0%, 100%)'
-    document.documentElement.style.setProperty(
-        '--theme-key-equals',
-        'hsl(25, 98%, 40%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-equals-shadow',
-        'hsl(25, 99%, 27%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-color-primary',
-        'hsl(60, 10%, 19%)'
-    )
-    document.querySelector('.attribution').style.color = 'hsl(0, 0%, 0%)'
+    const cls = ['theme-1', 'theme-3']
+    document.body.classList.add('theme-2')
+    document.body.classList.remove(...cls)
 }
 
 const updateTheme3 = () => {
     toggleSwitchBall.style.left = 'calc(100% - 3px)'
     toggleSwitchBall.style.transform = 'translateX(-100%)'
-    document.documentElement.style.setProperty(
-        '--color-primary',
-        'hsl(52, 100%, 62%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-primary',
-        'hsl(268, 75%, 9%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-screen',
-        'hsl(268, 71%, 12%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-bg-keypad',
-        'hsl(268, 71%, 12%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary',
-        'hsl(268, 47%, 21%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-primary-shadow',
-        'hsl(290, 70%, 36%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary',
-        'hsl(281, 89%, 26%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-secondary-shadow',
-        'hsl(285, 91%, 52%)'
-    )
-    keyEquals.style.color = 'hsl(198, 20%, 13%)'
-    document.documentElement.style.setProperty(
-        '--theme-key-equals',
-        'hsl(176, 100%, 44%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-key-equals-shadow',
-        'hsl(177, 92%, 70%)'
-    )
-    document.documentElement.style.setProperty(
-        '--theme-color-primary',
-        'hsl(52, 100%, 62%)'
-    )
-    document.querySelector('.attribution').style.color = 'hsl(52, 100%, 62%)'
+    const cls = ['theme-1', 'theme-2']
+    document.body.classList.add('theme-3')
+    document.body.classList.remove(...cls)
 }
+
+// Calc functions
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+// const outputEl = document.querySelector('#output')
+
+// const calculator = new Calculator(outputEl)
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', (button) => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', (button) => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', (button) => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
